16422124ecc7b52084af962da37efcb9d0a6873c
baihao <baih@ckt.cn>
2025-08-11 15:32:42 +0800

    提交项目

---
+++ account-data/program/src/lib.rs
@@ -0,0 +1,10 @@
+mod state;
+mod instructions;
+mod process;
+
+use solana_program::entrypoint;
+
+use process::process_instruction;
+
+// 智能合约的入口，客户端调用智能合约时，就是在调用这里指定的函数
+entrypoint!(process_instruction);
