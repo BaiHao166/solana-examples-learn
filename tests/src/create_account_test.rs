867c01bf5fb6244ce71f29480c43b540edc9d424
baihao <baihao166@163.com>
2025-09-01 14:41:06 +0800

    test

--- tests/src/create_account_test.rs
+++ tests/src/create_account_test.rs
@@ -1,8 +1,6 @@
-
-
 #[cfg(test)]
 mod test {
-    use std::str::FromStr;
+    use crate::{DEV_NET_URL, MY_WALLET_PRIVATE_KEY};
     use solana_client::nonblocking::rpc_client::RpcClient;
     use solana_sdk::commitment_config::CommitmentConfig;
     use solana_sdk::instruction::{AccountMeta, Instruction};
@@ -10,13 +8,13 @@ mod test {
     use solana_sdk::signature::{Keypair, Signer};
     use solana_sdk::system_program;
     use solana_sdk::transaction::Transaction;
-    use crate::{DEV_NET_URL, MY_WALLET_PRIVATE_KEY};
-
+    use std::str::FromStr;
 
     #[tokio::test]
     async fn test() {
         // 链接solana开发网
-        let client = RpcClient::new_with_commitment(DEV_NET_URL.to_string(), CommitmentConfig::confirmed());
+        let client =
+            RpcClient::new_with_commitment(DEV_NET_URL.to_string(), CommitmentConfig::confirmed());
 
         let wallet_keypair = Keypair::try_from(MY_WALLET_PRIVATE_KEY).unwrap();
 
@@ -31,8 +29,8 @@ mod test {
             vec![
                 AccountMeta::new(wallet_keypair.pubkey(), true),
                 AccountMeta::new(new_account_keypair.pubkey(), true),
-                AccountMeta::new_readonly(system_program::id(), false)
-            ]
+                AccountMeta::new_readonly(system_program::id(), false),
+            ],
         );
 
         // 3. 创建和签署交易
@@ -41,11 +39,14 @@ mod test {
             &[instruction],
             Some(&wallet_keypair.pubkey()),
             &[&wallet_keypair, &new_account_keypair],
-            hash
+            hash,
         );
 
         // 4. 发送并确认交易
-        let sign = client.send_and_confirm_transaction(&transaction).await.unwrap();
+        let sign = client
+            .send_and_confirm_transaction(&transaction)
+            .await
+            .unwrap();
 
         println!("签名：{}", sign);
     }
