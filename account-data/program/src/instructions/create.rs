f6c041600edd5f59cb2ecd2aabbcb47db84fea48
BH <baihao166@163.com>
2025-09-01 16:28:20 +0800

    test

--- account-data/program/src/instructions/create.rs
+++ account-data/program/src/instructions/create.rs
@@ -1,3 +1,4 @@
+use crate::state::address_info::AddressInfo;
 use borsh::{self, BorshSerialize};
 use solana_program::account_info::{next_account_info, AccountInfo};
 use solana_program::entrypoint::ProgramResult;
@@ -6,13 +7,12 @@ use solana_program::pubkey::Pubkey;
 use solana_program::rent::Rent;
 use solana_program::system_instruction;
 use solana_program::sysvar::Sysvar;
-use crate::state::address_info::AddressInfo;
 pub fn create_address_info(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
-    address_info: AddressInfo
+    address_info: AddressInfo,
 ) -> ProgramResult {
-    let account_iter= &mut accounts.iter();
+    let account_iter = &mut accounts.iter();
     let address_info_account = next_account_info(account_iter)?;
     let payer = next_account_info(account_iter)?;
     let system_program = next_account_info(account_iter)?;
@@ -24,11 +24,11 @@ pub fn create_address_info(
 
     // 创建一个存储街道地址的数据账户指令
     let instruction = system_instruction::create_account(
-        payer.key, // 创建数据账户时谁去支付费用
+        payer.key,                // 创建数据账户时谁去支付费用
         address_info_account.key, // 创建的数据账户的address
-        lamports_required, // 创建数据账户时需要支付的费用
-        space as u64, // 创建的数据账户需要的空间
-        program_id // 创建的数据账户的拥有者，这里是这个智能合约
+        lamports_required,        // 创建数据账户时需要支付的费用
+        space as u64,             // 创建的数据账户需要的空间
+        program_id,               // 创建的数据账户的拥有者，这里是这个智能合约
     );
 
     let array = [
@@ -42,8 +42,6 @@ pub fn create_address_info(
     let address_info_account_rc = &mut address_info_account.data.borrow_mut();
     let mut data = &mut address_info_account_rc[..];
     address_info.serialize(&mut data)?;
-
-
 
     Ok(())
 }
