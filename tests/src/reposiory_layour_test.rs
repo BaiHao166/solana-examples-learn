7697e8564f0e7a7db181781685e25e7921d3dcde
BH <baihao166@163.com>
2025-09-29 15:26:35 +0800

    测试仓库布局

--- tests/src/reposiory_layour_test.rs
+++ tests/src/reposiory_layour_test.rs
@@ -7,6 +7,7 @@ mod tests {
     use solana_sdk::instruction::{AccountMeta, Instruction};
     use solana_sdk::pubkey::Pubkey;
     use solana_sdk::signature::{Keypair, Signer};
+    use solana_sdk::system_program;
     use solana_sdk::transaction::Transaction;
 
     #[tokio::test]
@@ -17,28 +18,33 @@ mod tests {
         let my_wallet = Keypair::try_from(MY_WALLET_PRIVATE_KEY).unwrap();
 
         // 3. 创建指令
-        let program_id = Pubkey::try_from("7abRwRJE8V6gzMZHc7MUuABvZ2Tp6Pwu79C4aYVsJuLU").unwrap();
+        let program_id = Pubkey::try_from("oh7FHYtj9YCDajPVFH64J2jP3t5AhRHQvwdE4sD7Lyk").unwrap();
 
         let data = CarnivalInstructionData {
             name: "正在演戏的河北彩花".to_string(),
             height: 180,
             ticket_count: 10,
             attraction: "game".to_string(),
-            attraction_name: "三角洲行动".to_string(),
+            attraction_name: "Ring Toss".to_string(),
         };
 
         let instruction = Instruction::new_with_borsh(
             program_id,
             &data,
             vec![
-                AccountMeta::new(my_wallet.pubkey(), true)
+                AccountMeta::new(my_wallet.pubkey(), true),
+                AccountMeta::new_readonly(system_program::id(), false)
             ],
         );
 
         // 4. 创建和签署交易
         let hash = client.get_latest_blockhash().await.unwrap();
-        let mut transaction = Transaction::new_with_payer(&[instruction], Some(&my_wallet.pubkey()));
-        transaction.sign(&[my_wallet], hash);
+        let transaction = Transaction::new_signed_with_payer(
+            &[instruction],
+            Some(&my_wallet.pubkey()),
+            &[&my_wallet],
+            hash,
+        );
 
         // 5. 发送和确认交易
         let signature = client.send_and_confirm_transaction(&transaction).await.unwrap();
