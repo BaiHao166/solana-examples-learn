1570592b4f644a785ccf4031e569029445bf6688
BH <baihao166@163.com>
2025-09-29 14:54:45 +0800

    仓库布局

--- repository-layout/src/lib.rs
+++ repository-layout/src/lib.rs
@@ -1,3 +1,8 @@
+use crate::processor::process_instruction;
+use solana_program::entrypoint;
+
 pub mod modules;
 pub mod error;
 pub mod processor;
+
+entrypoint!(process_instruction);
--- repository-layout/src/processor.rs
+++ repository-layout/src/processor.rs
@@ -5,11 +5,9 @@ use crate::modules::instructions::{eat_food, get_on_ride, play_game};
 use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::account_info::AccountInfo;
 use solana_program::entrypoint::ProgramResult;
+use solana_program::msg;
 use solana_program::program_error::ProgramError;
 use solana_program::pubkey::Pubkey;
-use solana_program::{entrypoint, msg};
-
-entrypoint!(process_instruction);
 
 #[derive(BorshDeserialize, BorshSerialize, Debug)]
 pub struct CarnivalInstructionData {
